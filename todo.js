b3d4db99d7a0c9b5f7025219c165a66b44cb09f5
Ajay kumar gupta <96380335+ajay20001003003@users.noreply.github.com>
2023-10-18 09:17:33 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,15 +1,5 @@
-let todoList = [
-  {
-    item: 'Buy Milk',
-    dueDate: '4/10/2023'
-  },
-   {
-    item: 'Buy Milk',
-    dueDate: '4/10/2023'
-  }
-  
-];
-
+let todoList ;
+ 
 displayItems();
 
 function addTodo() {
@@ -17,26 +7,59 @@ function addTodo() {
   let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
   let todoDate = dateElement.value;
-  todoList.push({item: todoItem, dueDate: todoDate});
+  
+  todoList=JSON.parse(localStorage.getItem('userdetails'))??[];
+  console.log(todoList);
+  todoList.push({
+    'item':todoItem,
+    'dueDate':todoDate
+  })
+  localStorage.setItem("userdetails",JSON.stringify(todoList));
+  console.log(todoList);
+
+
   inputElement.value = '';
   dateElement.value = '';
   displayItems();
+}
+function removefun(index){
+    todoList=JSON.parse(localStorage.getItem('userdetails'))??[];
+    todoList.splice(index,1);
+      localStorage.setItem("userdetails",JSON.stringify(todoList));
+      displayItems();
+
+
 }
 
 function displayItems() {
   let containerElement = document.querySelector('.todo-container');
   let newHtml = '';
+   todoList=JSON.parse(localStorage.getItem('userdetails'))??[];
+
+
+
+
   for (let i = 0; i < todoList.length; i++) {
     let {item, dueDate} = todoList[i];
 
 
-    newHtml += `
-      <span>${item}</span>
-      <span>${dueDate}</span>
-      <button class='btn-delete' onclick="todoList.splice(${i}, 1);
-      displayItems();">Delete</button>
-    `;
-  }
-  containerElement.innerHTML = newHtml;
-}
+    // newHtml += `
+    //   <span>${item}</span>
+    //   <span>${dueDate}</span>
+    //    <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+    //   displayItems();">Delete</button>
+    // `;
+     newHtml += `
+    <span>${item}</span>
+     <span>${dueDate}</span>
+     <button class='btn-delete' onclick="removefun(${i})";
+    displayItems();">Delete</button>
+   `;
 
+
+   
+  }
+    localStorage.setItem("userdetails",JSON.stringify(todoList));
+  containerElement.innerHTML = newHtml;
+
+}
